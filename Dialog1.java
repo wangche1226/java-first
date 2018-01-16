57a7f8b11bbc133843283622ac7c96cc66c5d3f9
wangche1226 <31649086+wangche1226@users.noreply.github.com>
2018-01-16 18:53:22 +0800

    1.16.1

---
+++ Dialog1.java
@@ -0,0 +1,11 @@
+package GradeBookTest;
+
+import javax.swing.JOptionPane;
+
+public class Dialog1 
+{
+   public static void main( String args[] )
+   {
+	   JOptionPane.showMessageDialog( null,"Welcome\nto\njava");
+   }
+}
