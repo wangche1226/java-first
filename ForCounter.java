64813811d24af60dba5a5ddb05837bd97b2f926c
wangche1226 <31649086+wangche1226@users.noreply.github.com>
2018-01-18 20:35:27 +0800

    1.18.3

---
+++ ForCounter.java
@@ -0,0 +1,11 @@
+package GradeBookTest;
+
+public class ForCounter 
+{
+   public static void main( String args[] )
+   {
+	   for ( int counter = 1;counter <=10; counter++)
+		   System.out.printf("%d ", counter);
+	   System.out.println();
+   }
+}
