90738da6d279a26230d17f140596178f8ce765af
wangche1226 <31649086+wangche1226@users.noreply.github.com>
2018-01-18 20:25:51 +0800

    1.18.2

---
+++ WhileCounter.java
@@ -0,0 +1,15 @@
+package GradeBookTest;
+
+public class WhileCounter 
+{
+   public static void main( String args[] )
+   {
+	   int counter = 1;
+	   while( counter <= 10)
+	   {
+		   System.out.printf("%d ",counter );
+		   ++counter;
+	   }
+	   System.out.println();
+   }
+}
