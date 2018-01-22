2841fe6929f38e4c259bd17477900b81f118ec9c
wangche1226 <31649086+wangche1226@users.noreply.github.com>
2018-01-22 16:33:10 +0800

    1.22.1

---
+++ Mystery.java
@@ -0,0 +1,20 @@
+package GradeBookTest;
+
+public class Mystery
+{
+   public static void main(String args[] )
+   {
+	   int y;
+	   int x = 1;
+	   int total = 0;
+	   
+	   while( x <= 10 )
+	   {
+		   y = x*x;
+		   System.out.println(y);
+		   total += y;
+		   ++x;
+	   }
+	   
+   }
+}
