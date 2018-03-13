679943724808829cecf584227bb294d42b31ece5
wangche1226 <31649086+wangche1226@users.noreply.github.com>
2018-03-13 15:52:15 +0800

    18.3.13.3

--- Dog.java
+++ Dog.java
@@ -1,30 +1,33 @@
 
-public class Dog {
+class Dog {
+	int size;
 	String name;
-	public static void main (String[] args) {
-		Dog dog1 = new Dog();
-		dog1.bark();
-		dog1.name = "Bart";
-		
-		Dog[] myDogs = new Dog[3];
-		myDogs[0] = new Dog();
-		myDogs[1] = new Dog();
-		myDogs[2] = dog1;
-		
-		myDogs[0].name = "Fred";
-		myDogs[1].name = "marge";
-		
-		System.out.print("last dog's name is ");
-		System.out.println("myDogs[2].name");
-		
-		int x = 0;
-		while(x<myDogs.length) {
-			myDogs[x].bark();
-			x=x+1;
+	void bark() {
+		if (size>60) {
+			System.out.println("Woof!Woof!");
+		}else if(size>14) {
+			System.out.println("Ruff!Ruff!");
+		}else {
+			System.out.println("Yip!Yip!");
 		}
 	}
-	public void bark() {
-		System.out.println(name + " Says Ruff!");
+}
+public class DogTestDrive {
+	public static void main (String[] args) {
+		Dog one = new Dog();
+		one.size = 70;
+		Dog two = new Dog();
+		one.size = 8;
+		Dog three = new Dog();
+		one.size = 35;
+		
+		one.bark();
+		two.bark();
+		three.bark();
+		
 	}
+	
 
 }
+
+
